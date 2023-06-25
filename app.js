d12f6ba2407ba5125c43fd07a453517e78f850b6
Aashish Dhiman <aashishdhiman88@gmail.com>
2023-06-25 23:27:40 +0530

    added database to store data

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
-console.log(date());
+// console.log(date());
 require("dotenv").config();
 
 const app = express();
 const PORT = 3000 || process.env.PORT;
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.use(
     bodyParser.urlencoded({
         extended: true,
@@ -15,26 +16,68 @@ app.use(
 
 app.set("view engine", "ejs");
 
-const items = [];
-const workItems = [];
+mongoose.connect("mongodb://127.0.0.1:27017/toDoListDB", {
+    useNewUrlParser: true,
+});
 
-app.get("/", function (req, res) {
+const itemsSchema = new mongoose.Schema({
+    name: String,
+});
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name: "Welcome to ToDoList.",
+});
+const item2 = new Item({
+    name: "Hit + button to add new item.",
+});
+const item3 = new Item({
+    name: "Hit <-- button to delete item.",
+});
+
+const defaultItem = [item1, item2, item3];
+
+app.get("/", async function (req, res) {
     const day = date();
 
-    res.render("list", {
-        listTitle: day,
-        listItems: items,
-    });
+    const query = await Item.find({});
+    if (query.length === 0) {
+        Item.insertMany(defaultItem)
+            .then(function () {
+                console.log("Successfully saved items to DB");
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+        res.redirect("/");
+    } else {
+        // console.log(query);
+        res.render("list", {
+            listTitle: day,
+            listItems: query,
+        });
+    }
 });
 
 app.post("/", (req, res) => {
     // console.log(req.body);
-    const newItem = req.body.newItem;
+    const itemName = req.body.newItem;
+    const item = new Item({
+        name: itemName,
+    });
+
     if (req.body.list === "Work List") {
-        workItems.push(newItem);
+        // workItems.push(newItem);
         res.redirect("/work");
     } else {
-        items.push(newItem);
+        item.save()
+            .then(function () {
+                console.log("Successfully saved new item to DB");
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
         res.redirect("/");
     }
 });
@@ -44,6 +87,18 @@ app.get("/work", (req, res) => {
         listTitle: "Work List",
         listItems: workItems,
     });
+});
+
+app.post("/delete", async (req, res) => {
+    const itemID = req.body.checkbox;
+    await Item.deleteOne({ _id: itemID })
+        .then(function () {
+            console.log("Successfully deleted item from DB");
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+    res.redirect("/");
 });
 
 app.listen(PORT, function () {
